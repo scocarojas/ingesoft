ff5f90c22f934058f8a5ec986d39bc95bae7f6a7
Yan Carlos Certuche <yancertuche@gamil.com>
2018-11-14 17:43:20 -0500

    hola soy yo

---
+++ EnsayoInterfaz/js/menu.js
@@ -0,0 +1,5 @@
+var botmenu = document.getElementById('boton_menu');
+var navmenu = document.getElementById('nav');
+botmenu.addEventListener('click',function(){
+	navmenu.classList.toggle('mostrar');
+})
