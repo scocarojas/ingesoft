cee8771262c1bea2268441e1de08e8c2439d8ad7
Santiago Coca  Rojas <scocarojas@iMac-5.local>
2018-11-14 17:32:39 -0500

    Más nuevo

--- comunicacion.java
+++ comunicacion.java
@@ -8,8 +8,58 @@ public class Chat
     protected Usuario remitente;
     protected Director destinatario;
     protected String correoRemitente;
+    protected String contenido;
 
     /*Métodos*/
-    
-     
+
+    public void setIdChat(int idChat)
+    {
+    	this.idChat=idChat;
+    }
+
+    public int getIdChat()
+    {
+    	return this.idChat;
+    }
+
+    public void setRemitente(Usuario remitente)
+    {
+    	this.remitente=remitente;
+    }
+
+    public Usuario getRemitente()
+    {
+    	return this.remitente;
+    }
+
+	public void setDestinatario(Director destinatario)
+    {
+    	this.destinatario=destinatario;
+    }
+
+    public Director getDestinario()
+    {
+    	return this.destinatario;
+    }    
+
+    public void setCorreoRemitente(String correoRemitente)
+    {
+    	this.correoRemitente=correoRemitente;
+    }
+
+    public String getCorreoRemitente()
+    {
+    	return this.correoRemitente;
+    }
+
+    public void setContenido(String contenido)
+    {
+    	this.contenido=contenido;
+    }
+
+    public String getContenido()
+    {
+    	return this.contenido;
+    }
+
 }
